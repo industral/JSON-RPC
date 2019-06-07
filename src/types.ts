302e23d374cf16488abeec04a69164167cc729cd
Alex Ivasyuv <industral@gmail.com>
2019-06-07 17:15:48 +0300

    bump 0.2.0

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,15 @@ export interface IRequestParameter {
     method: string;
     params: object | [any]
 }
+
+export interface IJSONRCPResponse {
+    id: number;
+    jsonrpc: string;
+    result: string;
+    error?: object
+}
+
+export interface IReturn {
+    get(id?: number): IJSONRCPResponse;
+    getById(id: number): IJSONRCPResponse;
+}
